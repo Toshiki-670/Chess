45487a11a68cd01fef920f36945851fab9895a35
Toshiki <tsk@macbook.local>
2016-07-16 10:55:31 +0900

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Toshiki on 7/16/16.
+//  Copyright © 2016 Toshiki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
